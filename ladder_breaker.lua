646724458d25596fa7d181e1844ad2f86aaa77e4
m1kpe0 <yarikasik@inbox.ru>
2024-12-14 14:38:50 +0300

    Update ladder_breaker.lua

--- ladder_breaker.lua
+++ ladder_breaker.lua
@@ -1,6 +1,6 @@
 local hwid = game:GetService("RbxAnalyticsService"):GetClientId()
 if hwid == "00F343C7-EA99-466C-86CE-E2119D23B975" or "47c6b563-36f7-49b1-a144-4dfcb762049b" then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/m1kpe02/scripts/refs/heads/main/osk.lua"))
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/m1kpe02/scripts/refs/heads/main/osk.lua"))()
 else
 		local Player = game.Players.LocalPlayer
 		local numberValue = Instance.new("NumberValue") 
